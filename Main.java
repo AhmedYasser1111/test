5a865c443e4ccf035afccf5cc4fe6540fdc6dbc3
Islam-Hisham-2004 <152655552+Islam-Hisham-2004@users.noreply.github.com>
2023-12-09 17:24:32 +0200

    test

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+    Page p = new Page();
+    p.createPage();
+    p.createPage();
+
+
+      p.managePageSettings(true,false,true);
+        p.displayPageSettings();
+    }
+
+}
